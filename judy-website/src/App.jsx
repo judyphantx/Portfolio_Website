e39d22151fa09ff669b25a60d7089618ea61c335
Judy Phan <judy.phan2@g.austincc.edu>
2025-07-12 18:04:41 -0500

    Created mobile navigation bar

--- judy-website/src/App.jsx
+++ judy-website/src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 import "./index.css";
 
 function App() {
-  return(
-  <>
-    <LoadingScreen />
-  </>
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duraction-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
   );
 }
-
 
 export default App;
