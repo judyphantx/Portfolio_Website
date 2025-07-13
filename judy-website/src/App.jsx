ba546b208d2b105d3a3f000be407d3d0b28744c1
Judy Phan <judy.phan2@g.austincc.edu>
2025-07-12 21:34:46 -0500

    Home page with initial pop text

--- judy-website/src/App.jsx
+++ judy-website/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
 import "./index.css";
 
 function App() {
@@ -13,12 +14,13 @@ function App() {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
       <div
-        className={`min-h-screen transition-opacity duraction-700 ${
+        className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
---
+++ judy-website/src/components/sections/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative bg-black text-center px-4"
+    >
+      <div className="text-center z-10 px-4">
+        <h1 className="text-4xl font-bold mb-6 gradient-text leading-right">
+          Hi, I'm Judy Phan
+        </h1>
+      </div>
+    </section>
+  );
+};
